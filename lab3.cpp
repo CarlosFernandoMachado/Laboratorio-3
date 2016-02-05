7ce4c1a3fdf9dceca74a194d142714a080fc22c5
CarlosMachado <testsubjectno2@hotmail.com>
2016-02-05 15:23:25 -0600

    agregando funciones

--- lab3.cpp
+++ lab3.cpp
@@ -7,16 +7,17 @@ using std::cin;
 using std::endl;
 
 void ejercicio1(int magic, int score[], int size);
+void arreglar(int score[], int size);
 
 int main(int argc, char*argv[]){
 	bool seguir = true;
 	int menu;
+	srand(time(NULL));
 	int score[10] = {100,100,100,100,100,100,100,100,100,100};
 	while (seguir){
 		cout << "1: Ejercicio 1\n2: Ejercicio 2\n3: Salir" << endl;
 		cin >> menu;
 		if(menu == 1){
-			srand(time(NULL));
 			int magic = rand() % 1001 - 500;
 			ejercicio1(magic,score,10);
 			cout << "Si desea seguir presione 1: ";
@@ -25,6 +26,30 @@ int main(int argc, char*argv[]){
 				seguir = false;
 		}else if(menu == 2){
 			int stats[4][3];
+			int f = 4, c = 3;
+			for (int i = 0 ; i < f ; i++){
+				for (int j = 0 ; j < c ; j++){
+					if (j == 0)//85-115
+						stats[i][j] = rand() % 116 + 85;
+					if (j == 1)//50-75
+						stats[i][j] = rand() % 76 + 50;
+					if (j == 2)//150-200
+						stats[i][j] = rand() % 201 + 150;
+				}
+			}
+			for (int i = 0 ; i < f ; i++){
+				for (int j = 0 ; j < c ; j++){
+					if (j == c - 1)
+						cout << stats[i][j] << "\n";
+					else
+						cout << stats[i][j] << "--";
+				}
+			}
+			//ejercicio2(stats,f,c);
+			cout << "Si desea seguir presione 1: ";
+			cin >> menu;
+			if (menu != 1)
+				seguir = false;
 		}else if(menu == 3){
 			seguir = false;
 		}
@@ -44,16 +69,7 @@ void ejercicio1(int magic, int score[], int size){
 			cout << "Felicidades has ganado\nSu score es: " << cont << endl;
 			if (cont < score[9])
 				score[9] = cont;
-			int Temp;
-			for(int i = 0 ; i < size ; i++){
-				for(int j = 0 ; j < size - 1 ; j++){
-					if( score[j] > score[j + 1] ){
-						Temp = score[j];
-						score[j] = score[j + 1];
-						score[j + 1] = Temp;
-					}
-				}
-			}
+			arreglar(score,size);
 			for (int i = 0 ; i < size ; i++){
 				cout <<  i + 1 <<": " << score[i] << endl;
 			}
@@ -69,9 +85,15 @@ void ejercicio1(int magic, int score[], int size){
 	}
 
 }
-void ejercicio2(int stats[][], int f, int c){
-
-
-
-
+void arreglar(int score[], int size){
+	int Temp;
+	for(int i = 0 ; i < size ; i++){
+		for(int j = 0 ; j < size - 1 ; j++){
+			if( score[j] > score[j + 1] ){
+				Temp = score[j];
+				score[j] = score[j + 1];
+				score[j + 1] = Temp;
+			}
+		}
+	}
 }
